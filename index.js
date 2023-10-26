f9767fc97435f0b03bf48d3064508e546119fd06
hossain-iit-12 <aalhossain1999@gmail.com>
2023-10-26 12:14:02 +0600

    upload

---
+++ index.js
@@ -0,0 +1,13 @@
+var typed=new Typed(".typing",{
+    strings:["a web developer"," an app developer"," a graphic Designer"],
+typeSpeed:100,
+BackSpeed:60,
+loop:true
+})
+// var options = {
+//     strings: ['Puja', 'UI Desiner',  'Web Developer'],
+//     typeSpeed: 100,
+//     backSpeed:100,
+//     loop: true
+//   };
+// var typed = new Typed('.typing', options);
